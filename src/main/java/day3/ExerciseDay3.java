3749f5157648b1cd92b679e8ee3a49c307afc031
nurhidayatulf <nurhidayatulfatihah19@gmail.com>
2024-09-04 14:38:25 +0700

    feat: add answer of question 4

--- src/main/java/day3/ExerciseDay3.java
+++ src/main/java/day3/ExerciseDay3.java
@@ -32,6 +32,40 @@ public class ExerciseDay3 {
             }
         }
         System.out.println("Largest element in the array is " + max);
+
+        //4. Implement a simple calculator that can perform addition, subtraction, multiplication, and division.
+        int operator, firstNumber, secondNumber;
+
+        System.out.println("1 - Add \n 2 - Substract \n 3 - Multiply \n 4 - Divide");
+        System.out.println("Choose the Operator: ");
+        Scanner inputScanner3 = new Scanner(System.in);
+        operator = inputScanner3.nextInt();
+
+        System.out.println("Enter the first number : ");
+        firstNumber = inputScanner3.nextInt();
+
+        System.out.println("Enter the second number : ");
+        secondNumber = inputScanner3.nextInt();
+
+        int result = 0;
+        switch (operator) {
+            case 1:
+                result = firstNumber + secondNumber;
+                break;
+            case 2:
+                result = firstNumber - secondNumber;
+                break;
+            case 3:
+                result = firstNumber * secondNumber;
+                break;
+            case 4:
+                result = firstNumber / secondNumber;
+                break;
+
+            default:
+                System.out.println("Entered operator is invalid");
+        }
+        System.out.println("Result : " + result);
     }
 
     public static int SumOfDigits(int inputNumber) {
