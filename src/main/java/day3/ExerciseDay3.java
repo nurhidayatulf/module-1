44875774e14c9c75fc147b443fe4495200eb343d
nurhidayatulf <nurhidayatulfatihah19@gmail.com>
2024-09-04 14:17:22 +0700

    feat: add answer of question 3

--- src/main/java/day3/ExerciseDay3.java
+++ src/main/java/day3/ExerciseDay3.java
@@ -6,14 +6,14 @@ public class ExerciseDay3 {
     public static void main(String[] args) {
         //1. Write a program to calculate the sum of digits in a given number.
         Scanner inputScanner = new Scanner(System.in);
-        System.out.print("Input number: ");
+        System.out.print("Input number to sum the digits: ");
         int inputNumber = inputScanner.nextInt();
 
         System.out.println("Sum of " + inputNumber + " is " + SumOfDigits(inputNumber));
 
         //2. Create a function to check if a number is prime.
         Scanner inputScanner2 = new Scanner(System.in);
-        System.out.print("Input number: ");
+        System.out.print("Input number to prime check: ");
         int inputNumber2 = inputScanner2.nextInt();
 
         if (isPrime(inputNumber2)) {
@@ -21,6 +21,17 @@ public class ExerciseDay3 {
         } else {
             System.out.println(inputNumber2 + " is not a prime number");
         }
+
+        //3. Write a program to find the largest element in an array.
+        int[] arrayNumber = new int[] {3, 7, 2, 8, 1};
+        int max = arrayNumber[0];
+
+        for (int i = 1; i < arrayNumber.length; i++) {
+            if (arrayNumber[i] > max) {
+                max = arrayNumber[i];
+            }
+        }
+        System.out.println("Largest element in the array is " + max);
     }
 
     public static int SumOfDigits(int inputNumber) {
