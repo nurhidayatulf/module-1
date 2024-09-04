952b9ecd751ca1e229c7b585978cbea7e711228d
nurhidayatulf <nurhidayatulfatihah19@gmail.com>
2024-09-04 14:04:13 +0700

    feat: add answer of question 2

--- src/main/java/day3/ExerciseDay3.java
+++ src/main/java/day3/ExerciseDay3.java
@@ -6,11 +6,21 @@ public class ExerciseDay3 {
     public static void main(String[] args) {
         //1. Write a program to calculate the sum of digits in a given number.
         Scanner inputScanner = new Scanner(System.in);
-        System.out.print("Input numbers: ");
-
+        System.out.print("Input number: ");
         int inputNumber = inputScanner.nextInt();
+
         System.out.println("Sum of " + inputNumber + " is " + SumOfDigits(inputNumber));
 
+        //2. Create a function to check if a number is prime.
+        Scanner inputScanner2 = new Scanner(System.in);
+        System.out.print("Input number: ");
+        int inputNumber2 = inputScanner2.nextInt();
+
+        if (isPrime(inputNumber2)) {
+            System.out.println(inputNumber2 + " is a prime number");
+        } else {
+            System.out.println(inputNumber2 + " is not a prime number");
+        }
     }
 
     public static int SumOfDigits(int inputNumber) {
@@ -24,5 +34,17 @@ public class ExerciseDay3 {
         }
 
         return sum;
+    }
+
+    public static boolean isPrime (int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
