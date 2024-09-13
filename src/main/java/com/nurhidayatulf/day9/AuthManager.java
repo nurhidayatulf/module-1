ab549780bd6ab25420f0717fcd72f7183ac6cc3b
nurhidayatulf <nurhidayatulfatihah19@gmail.com>
2024-09-13 08:41:17 +0700

    feat: more answer day 9

---
+++ src/main/java/com/nurhidayatulf/day9/AuthManager.java
@@ -0,0 +1,54 @@
+package com.nurhidayatulf.day9;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class AuthManager {
+    private final Map<String, User> users;
+    private User loggedInUser;
+
+    public AuthManager() {
+        this.users = new HashMap<>();
+    }
+
+    public void registerUser(Scanner scanner) {
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
+        if (users.containsKey(username)) {
+            System.out.println("Username already exists. Try again.");
+        } else {
+            User newUser = new User(username, password);
+            users.put(username, newUser);
+            System.out.println("Registration successful!");
+        }
+    }
+
+    public boolean loginUser(Scanner scanner) {
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
+        User user = users.get(username);
+        if (user != null && user.getPassword().equals(password)) {
+            loggedInUser = user;
+            System.out.println("Login successful!");
+            return true;
+        } else {
+            System.out.println("Invalid username or password. Try again.");
+            return false;
+        }
+    }
+
+    public User getLoggedInUser() {
+        return loggedInUser;
+    }
+
+    public void logout() {
+        loggedInUser = null;
+    }
+}
